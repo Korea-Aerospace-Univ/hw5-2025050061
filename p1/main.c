35311a35a03294631a7af897f71c8bed59baff5f
2025050061 <chaeekimpd0215@gmail.com>
2026-04-29 15:58:49 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -8,9 +8,8 @@ int main(void)
     scanf("%d", &N);
     
     for(int i=1; i<=N/900; i++) {
-        for(int j=2; j<=N/750; j+=2) {
-                
-            for(int k=1; k<=N/200; k++) {
+        for(int j=2; j<=(N-900*i)/750; j+=2) {
+            for(int k=1; k<=(N-900*i-750*j)/200; k++) {
                 if (N-900*i-750*j-200*k == 0) {
                     if (k<i || k<j) {
                         printf("%d %d %d\n", i, j, k);
