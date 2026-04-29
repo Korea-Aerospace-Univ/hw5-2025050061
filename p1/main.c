7e4494323eb7dd7c7c8ffba83e1496ff1f1f6e12
2025050061 <chaeekimpd0215@gmail.com>
2026-04-29 15:50:28 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,29 +3,24 @@
 int main(void)
 {
     int N;
-    bool is_true = false;
     int x=0, y=0, z=0;
     
     scanf("%d", &N);
     
     for(int i=1; i<=N/900; i++) {
-        is_true = false;
-        
-        for(int j=2; j<=(N-900*i)/750; j+=2) {
+        for(int j=2; j<=N/750; j+=2) {
                 
-            for(int k=1; k<=(N-900*i-750*j)/200; k++) {
+            for(int k=1; k<=N/200; k++) {
                 if (N-900*i-750*j-200*k == 0) {
-                    if (k<i || k<j)
-                        is_true = true;
+                    if (k<i || k<j) {
                         x = i; y = j; z=k;
+                        printf("%d %d %d\n", x, y, z);
+                    }
                 }
             }
         }
-    
-    if (is_true)
-        printf("%d %d %d\n", x, y, z);
     }
-    if (!x ||!y || !z) 
+    if (!x) 
         printf("none");
         
     return 0;
